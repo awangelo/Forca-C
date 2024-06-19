515e621a32e65ffd866608c95f8be5b5f000e8f9
awangelo <anggg.gaio@gmail.com>
2024-06-18 22:44:42 -0300

    add base do jogo

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char[50] palavra = "Ola monitoria!";
+void menu();
+void gameStart();
+
+char palavra[20];
 
 int main() {
-    printf("Ola monitoria!\n");
+    menu();
+    gameStart();
     return 0;
+}
+
+void menu() {
+    clearConsole();
+    printf("Digite uma palavra para ser adivinhada: ");
+    fgets(palavra, 20, stdin);
+    printf("Iniciando jogo com a palavra: %s ", palavra);
+    system("sleep 3");
+
+    printf("\n");
+}
+
+void gameStart() {
+
+}
+
+void clearConsole() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
